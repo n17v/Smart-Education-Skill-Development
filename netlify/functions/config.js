f906e39d7d13594b43ebd3c3aab5530a18258589
Dhairya <patel.dhairya179@gmail.com>
2026-09-19 02:32:12 +0530

    Create config.js

---
+++ netlify/functions/config.js
@@ -0,0 +1,10 @@
+exports.handler = async () => {
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      supabaseUrl: process.env.SUPABASE_URL || "",
+      supabaseAnonKey: process.env.SUPABASE_ANON_KEY || ""
+    })
+  };
+};
